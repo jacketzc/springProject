1810bec41a42decace57f75527fb86e256bc7895
jacketzc <jacketzc@outlook.com>
2019-11-23 18:21:47 +0800

    laptop：更新到spring半自动配置事务

--- src/com/jacketzc/test/Lesson04.java
+++ src/com/jacketzc/test/Lesson04.java
@@ -1,11 +1,18 @@
 package com.jacketzc.test;
 
 import com.jacketzc.service.AccountService;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Lesson04 {
     public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("beans8.xml");
+        AccountService service = (AccountService) context.getBean("service");
+        service.transer("jacketzc","xiaoming",10);
+    }
+    @Test
+    public void test1(){
         ApplicationContext context=new ClassPathXmlApplicationContext("beans8.xml");
         AccountService service = (AccountService) context.getBean("service");
         service.transer("jacketzc","xiaoming",10);
